f521acf8cab562ebb245b22b26816d306109b697
fusion809 <brentonhorne77@gmail.com>
2016-03-17 18:41:16 +1000

    Updating scripts

---
+++ install-custom.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+# This script is for automating the installation of Arch Linux and getting it ready for use on a laptop, potentially, with a Broadcom chip.
+
+echo "Welcome to fusion809's automated Arch Linux installer, forked from https://github.com/elasticdog/packer-arch's install-base.sh script."
+
+. "scripts/variables.sh"
+
+. "scripts/disks.sh"
+
+# Mount partition to target directory
+echo "==> mounting ${ROOT_PARTITION} to ${TARGET_DIR}"
+/usr/bin/mount -o noatime,errors=remount-ro ${ROOT_PARTITION} ${TARGET_DIR}
+
+# Bootstrapping base installation.
+echo '==> bootstrapping the base installation'
+/usr/bin/pacstrap ${TARGET_DIR} base base-devel
+. "scripts/package-custom.sh"
+
+. "scripts/bootloader.sh"
+
+echo '==> generating the filesystem table'
+/usr/bin/genfstab -p ${TARGET_DIR} >> "${TARGET_DIR}/etc/fstab"
+
+. "scripts/config-custom.sh"
+
+echo '==> adding workaround for shutdown race condition'
+/usr/bin/install --mode=0644 poweroff.timer "${TARGET_DIR}/etc/systemd/system/poweroff.timer"
+
+echo '==> installation complete!'
+/usr/bin/sleep 3
+/usr/bin/umount ${TARGET_DIR}
+/usr/bin/systemctl reboot
---
+++ scripts/config-custom.sh
@@ -0,0 +1,46 @@
+echo '==> generating the system configuration script'
+/usr/bin/install --mode=0755 /dev/null "${TARGET_DIR}${CONFIG_SCRIPT}"
+
+cat <<-EOF > "${TARGET_DIR}${CONFIG_SCRIPT}"
+	echo '${FQDN}' > /etc/hostname
+	/usr/bin/ln -s /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
+	echo 'KEYMAP=${KEYMAP}' > /etc/vconsole.conf
+	/usr/bin/sed -i 's/#${LANGUAGE}/${LANGUAGE}/' /etc/locale.gen
+	/usr/bin/locale-gen
+	/usr/bin/mkinitcpio -p linux
+	/usr/bin/usermod --password ${ROOT_PASSWORD} root
+
+	# Vagrant-specific configuration
+	/usr/bin/useradd --password ${USER_PASSWORD} --comment 'administrative user' --create-home --gid users ${USERNAME}
+  /usr/bin/gpasswd -a ${USERNAME} wheel
+
+  /usr/bin/sed -i "s/^#%wheel/%wheel/g" /etc/sudoers
+
+  echo "[home_fusion809_arch_extra_Arch_Extra]
+SigLevel = Never
+Server = http://download.opensuse.org/repositories/home:/fusion809:/arch_extra/Arch_Extra/$arch" >> /etc/pacman.conf
+  /usr/bin/pacman -Syu --noconfirm
+	MY_PACKS="broadcom-wl-dkms bumblebee docker kde-applications-meta octave plasma-meta sagemath virtualbox xf86-video-intel xf86-video-nouveau xorg yaourt"
+  /usr/bin/pacman -S ${MY_PACKS} --noconfirm
+	/usr/bin/usermod -a -G $USERNAME bumblebee docker vboxusers video
+	/usr/bin/systemctl enable docker
+	/usr/bin/systemctl enable sddm
+
+	/usr/bin/chsh -s /bin/zsh $USERNAME
+	/usr/bin/chsh -s /bin/zsh
+	cd /home/$USERNAME
+	/usr/bin/git clone https://github.com/robbyrussell/oh-my-zsh .oh-my-zsh
+	/usr/bin/mkdir GitHub
+	/usr/bin/git clone https://github.com/fusion809/arch-scripts GitHub/arch-scripts
+	/usr/bin/cp -a GitHub/arch-scripts/{Shell,.bashrc,.zshrc} .
+	/usr/bin/cp -a GitHub/arch-scripts/root/{Shell,.bashrc,.zshrc} /root/
+	/usr/bin/git clone https://github.com/fusion809/zsh-theme GitHub/zsh-theme
+	/usr/bin/cp -a GitHub/zsh-theme/{hcompat,hornix}.zsh-theme .oh-my-zsh/themes/
+
+	# clean up
+	/usr/bin/pacman -Rcns --noconfirm gptfdisk
+EOF
+
+echo '==> entering chroot and configuring system'
+/usr/bin/arch-chroot ${TARGET_DIR} ${CONFIG_SCRIPT}
+rm "${TARGET_DIR}${CONFIG_SCRIPT}"
